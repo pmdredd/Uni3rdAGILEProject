a15dd10708b006cb722a222ef669938269a94038
Jack Bremner <jack@banntech.co.uk>
2019-11-09 14:29:38 +0000

    remove test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'database/dbconnection.php';
-// $userExists = DB::run("SELECT COUNT(1) FROM users WHERE email = ?", ['test'])->fetchColumn();
-$user = DB::run("SELECT * from users where email = ?", ["test@tesom"])->fetch(PDO::FETCH_ASSOC);
-
-
-// var_dump($userExists);
-var_dump($user);
