16816256430faf796c5a6ad29ebc4af1713fad9c
Jack Bremner <jack@banntech.co.uk>
2019-11-11 20:25:27 +0000

    add student functions and create/details/delete, separate header into separate file, move js and css to own directory

---
+++ students/register_student.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'student_functions.php';
+
+session_start();
+$errors = array();
+
+if (isset($_POST['name'])) {
+
+    $name = $_POST['name'];
+    if (empty($name)) {
+        array_push($errors, "Student name is required");
+    }
+
+    if (count($errors) == 0) {
+        createStudent($name);
+        $_SESSION['success'] = true;
+        header('location: all_students.php');
+    } else {
+        foreach ($errors as $error) {
+            echo '<p>' . $error . '</p>';
+        }
+        echo "<p><a href='register.html'>Please try again</a></p>";
+    }
+}
