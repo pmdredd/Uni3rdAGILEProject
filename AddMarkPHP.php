defdc069491d3f0046de522d401b0b458314fbcf
Lairdd1989 <54322466+Lairdd1989@users.noreply.github.com>
2019-11-11 11:36:18 +0000

    Minor Fixes
    Formatting and Names of $_POST

--- AddMarkPHP.php
+++ AddMarkPHP.php
@@ -10,85 +10,109 @@
         require_once 'database/dbconnection.php'; // $connection
         
         $Mark =  $_POST["Marks"];
-        $Attempt2 = $_POST["AttemptSecond"];
+        $Attempt2 = $_POST["Attempt"];
         $Student_ID = $_POST["StudentID"];
         $Course_ID = $_POST["CourseID"];
-        $Grade = get_alphanumeric_grade($Mark, $Attempt2);
+        $Grade = get_alphanumeric_grade($Mark, $Attempt2);       
         
-        
-    function get_alphanumeric_grade ($Mark,$Attempt2) {
-        If ($Attempt2 === true){
-            If ($_POST["Marks"] >= 40) {
-                return "D3"; }
+        function get_alphanumeric_grade ($Mark,$Attempt2) {
+            If ($Attempt2 === true){
+                If ($_POST["Marks"] >= 40) {
+                    return "D3";
+                }
+                elseif if($_POST["Marks"] >= 37) {
+                    return "MF1";
+                }
+                elseif if($_POST["Marks"] >= 34) {
+                    return "MF2";
+                }
+                elseif if($_POST["Marks"] >= 30) {
+                    return "MF3";
+                }
+                elseif if($_POST["Marks"] >= 20) {
+                    return "CF";
+                }
+                 elseif if($_POST["Marks"] >= 0) {
+                    return "BF";
+                }
+            }
+   
+            if($_POST["Marks"] >= 95)  {
+                return "A1";
+            }
+            elseif if($_POST["Marks"] >= 89) {
+                return "A2";
+            }
+            elseif if($_POST["Marks"] >= 83) {
+                return "A3";
+            }
+            elseif if($_POST["Marks"] >= 76) {
+                return "A4";
+            }
+            elseif if($_POST["Marks"] >= 70) {
+                return "A5";
+            }
+            elseif if($_POST["Marks"] >= 67) {
+                return "B1";
+            }
+            elseif if($_POST["Marks"] >= 64) {
+                return "B2";
+            }
+            elseif if($_POST["Marks"] >= 60) {
+                return "B3";
+            }
+            elseif if($_POST["Marks"] >= 57) {
+                return "C1";
+            }
+            elseif if($_POST["Marks"] >= 54) {
+                return "C2";
+            }
+            elseif if($_POST["Marks"] >= 50) {
+                return "C3";
+            }
+            elseif if($_POST["Marks"] >= 47) {
+                return "D1";
+            }
+            elseif if($_POST["Marks"] >= 44) {
+                return "D2";
+            }
+            elseif if($_POST["Marks"] >= 40) {
+                return "D3";
+            }
             elseif if($_POST["Marks"] >= 37) {
-                return "MF1"; }
+                return "MF1";
+            }
             elseif if($_POST["Marks"] >= 34) {
-                return "MF2"; }
+                return "MF2";
+            }
             elseif if($_POST["Marks"] >= 30) {
                 return "MF3";
+            }
             elseif if($_POST["Marks"] >= 20) {
-                return "CF"; }
+                return "CF";
+            }
              elseif if($_POST["Marks"] >= 0) {
-                return "BF"; }
+                return "BF";
+            }
         }
-   
-        if($_POST["Marks"] >= 95)  {
-            return "A1"; }
-        elseif if($_POST["Marks"] >= 89) {
-            return "A2"; }
-        elseif if($_POST["Marks"] >= 83) {
-            return "A3"; }
-        elseif if($_POST["Marks"] >= 76) {
-            return "A4"; }
-        elseif if($_POST["Marks"] >= 70) {
-            return "A5"; }
-        elseif if($_POST["Marks"] >= 67) {
-            return "B1"; }
-        elseif if($_POST["Marks"] >= 64) {
-            return "B2"; }
-        elseif if($_POST["Marks"] >= 60) {
-            return "B3"; }
-        elseif if($_POST["Marks"] >= 57) {
-            return "C1"; }
-        elseif if($_POST["Marks"] >= 54) {
-            return "C2"; }
-        elseif if($_POST["Marks"] >= 50) {
-            return "C3"; }
-        elseif if($_POST["Marks"] >= 47) {
-            return "D1"; }
-        elseif if($_POST["Marks"] >= 44) {
-            return "D2"; }
-        elseif if($_POST["Marks"] >= 40) {
-            return "D3"; }
-        elseif if($_POST["Marks"] >= 37) {
-            return "MF1"; }
-        elseif if($_POST["Marks"] >= 34) {
-            return "MF2"; }
-        elseif if($_POST["Marks"] >= 30) {
-            return "MF3";
-        elseif if($_POST["Marks"] >= 20) {
-            return "CF"; }
-         elseif if($_POST["Marks"] >= 0) {
-            return "BF"; }
-    }
             
-    function submit_Submission($Student_ID, $Course_ID, $Mark, $Grade){
-    /*    $sql = "INSERT INTO TABLENAME (StudentID,CourseID, Marks, 
+        function submit_Submission($Student_ID, $Course_ID, $Mark, $Grade){
+            /*$sql = "INSERT INTO TABLENAME (StudentID,CourseID, Marks, 
             Grade, Attempt, SubmissionDate) VALUES ("
-               $_POST["StudentID"] . "', '" .
-               $_POST["CourseID"] . "', '" .
-               $_POST["Marks"]. "', " .
-               $Grade . "', " 
-              . "CURDATE()"
-               .")"    ;
-     */   
-     
-        $submission = DB::run("INSERT INTO TABLENAME (StudentID,CourseID, Marks, 
-            Grade, Attempt, SubmissionDate) VALUES (?,?,?,?,?, CURDATE()", [$Student_ID], [$Course_ID], [$Mark], [$Grade], [$Attemp2])->fetch();
-        return $submission;
-    }
+            $_POST["StudentID"] . "', '" .
+            $_POST["CourseID"] . "', '" .
+            $_POST["Marks"]. "', " .
+            $Grade . "', " 
+            . "CURDATE()"
+            .")";
+            */    
+            $submission = DB::run("INSERT INTO TABLENAME (StudentID,CourseID, Marks, 
+                Grade, Attempt, SubmissionDate) VALUES (?,?,?,?,?, CURDATE()", 
+                [$Student_ID], [$Course_ID], [$Mark], [$Grade], [$Attemp2])->fetch();
+            return $submission;
+        }
             
-           $query_successful = mysqli_query($connection, $sql);        
+        $query_successful = mysqli_query($connection, $sql);        
         if ($query_successful) {
             echo '<p>Student Marks Sumbitted Sucessfully</p>';
             echo '<p><a href="SystemHome.php">Home</a></p>';
