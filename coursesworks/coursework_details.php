1326bd79a78ee55cbae0c7f454c9ea1a4ec4080a
Jack Bremner <jack@banntech.co.uk>
2019-11-17 17:52:59 +0000

    add courseworks functionality

---
+++ coursesworks/coursework_details.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'course_functions.php';
+
+$course_id = htmlspecialchars($_GET["id"]);
+$course = getCourseById($course_id);
+
+if(isset($_POST['delete'])){ //check if form was submitted
+
+    deleteCourseById($course_id);
+    header('location: all_courses.php');
+}    
+
+if ($course) {
+    echo '<h1>' . $course['name'] . '</h1>';
+    echo '<br>';
+    echo '<button type="button" onclick="javascript:history.back()">Back</button>';
+    echo '<form action="" method="post">
+            <input type="hidden" name="delete" value="' . $course_id . '"/>
+            <input type="submit" value="Delete Course">
+        </form>'; 
+    echo '<br>';
+    echo '<br>';
+    echo 'List of courseworks for this course would be here';
+} else {
+    echo 'There was a problem, please try again';
+}
+
+?>
