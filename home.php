918f3f9338c9f643c6c0afdaa4bdcf91eb7b2977
Jack Bremner <j.z.bremner@dundee.ac.uk>
2019-11-18 09:45:16 +0000

    fix text in home.php

--- home.php
+++ home.php
@@ -6,6 +6,6 @@ require_once 'header.html';
   <h2>Welcome to the Course Manager System!</h2>
   <p>Click on 'Courses' to see all the Courses, and add new Courses</p>
   <p>Click on 'Courseworks' to see all the Courseworks, and add new Courseworks</p>
-  <p>Click on 'Courses' to see all the Students, and register a new Student</p>
-  <p>Click on 'Courses' to see all the Submissions, and add a new Submissions</p>
+  <p>Click on 'Students' to see all the Students, and register a new Student</p>
+  <p>Click on 'Submissions' to see all the Submissions, and add a new Submissions</p>
 </div>
