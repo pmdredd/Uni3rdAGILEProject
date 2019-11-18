b74fc9aeb7b2aaf5265018cbb3cc987763d4e1bc
Jack Bremner <j.z.bremner@dundee.ac.uk>
2019-11-18 13:45:37 +0000

    add comment for future reference and make layout clearer

--- submissions/all_submissions.php
+++ submissions/all_submissions.php
@@ -14,9 +14,12 @@ if (isset($_SESSION['success'])) {
 
 $submissions = getAllSubmissions();
 
+// we should try and group by coursework_id e.g. we first get a list of all submissions for Agile Development,
+// then a list of all submissions for Java Programming, then Business Systems, etc. see PDO::FETCH_GROUP
+
 if ($submissions) {
     foreach ($submissions as $submission) {
-        echo '<h2>' . $submission['coursework_name'] . '</h2>';
+        echo '<h3>Coursework: ' . $submission['coursework_name'] . '</h3>';
         echo "<a href='submission_details.php?id=" . $submission['submission_id'] . "'> Student : " . $submission['student_name'] 
               . "  Hand in date: " . $submission['hand_in_date'] . "</a>\n";
         echo "</br>";
