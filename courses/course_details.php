43bf01bdcb7f44bb48e76a785572f6c7bc724723
Jack Bremner <j.z.bremner@dundee.ac.uk>
2019-11-18 12:40:13 +0000

    add function to get all courseworks for a course and add to course details page

--- courses/course_details.php
+++ courses/course_details.php
@@ -4,6 +4,7 @@ require_once '../header.html';
 
 $course_id = htmlspecialchars($_GET["id"]);
 $course = getCourseById($course_id);
+$courseworks = getCourseworksByCourseId($course_id);
 
 if(isset($_POST['delete'])){ //check if form was submitted
 
@@ -14,14 +15,22 @@ if(isset($_POST['delete'])){ //check if form was submitted
 if ($course) {
     echo '<h1>' . $course['name'] . '</h1>';
     echo '<br>';
-    echo '<button type="button" onclick="javascript:history.back()">Back</button>';
     echo '<form action="" method="post">
             <input type="hidden" name="delete" value="' . $course_id . '"/>
             <input type="submit" value="Delete Course">
         </form>'; 
+    echo '<button type="button" onclick="javascript:history.back()">Back</button>';
     echo '<br>';
     echo '<br>';
-    echo 'List of courseworks for this course would be here';
+    echo '<h1>Courseworks for this Course</h1>';
+    if ($courseworks) {
+        foreach ($courseworks as $coursework) {
+            echo "<a href='../coursework_details.php?id=" . $coursework['coursework_id'] . "'>" . $coursework['name'] . "</a>";
+            echo "</br>";
+        }
+    } else {
+        echo '<p>This Course has no Courseworks yet</p>';
+    }
 } else {
     echo 'There was a problem, please try again';
 }
--- courses/course_functions.php
+++ courses/course_functions.php
@@ -26,3 +26,9 @@ function deleteCourseById($course_id) {
     $course = DB::run($query, [$course_id]);
     return $course;
 }
+
+function getCourseworksByCourseId($course_id) {
+    $query = "SELECT * FROM courseworks WHERE course_id=?";
+    $courseworks = DB::run($query, [$course_id])->fetchAll(PDO::FETCH_ASSOC);
+    return $courseworks;
+}
