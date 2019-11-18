dcc69e8eaf940962e127329a346004ffff1d750d
Jack Bremner <jack@banntech.co.uk>
2019-11-18 21:38:36 +0000

    replace old comments in CourseTest with DocBlocks

--- tests/CoursesTest.php
+++ tests/CoursesTest.php
@@ -3,21 +3,34 @@
 use PHPUnit\Framework\TestCase;
 require_once 'courses/course_functions.php';
 
+/**
+ * Class CoursesTest
+ */
 final class CoursesTest extends TestCase {
 
+    /**
+     * Create the test course on which the test methods are run.
+     * This will be run before each test method.
+     */
     protected function setUp(): void {
         DB::run("INSERT INTO courses (name) VALUES ('test course')");
     }
 
-    // will only actually check if the courses table is empty,
-    // not if the rows it returns are correct (e.g. wrong table)
+    /**
+     * Test the getAllCourses() function which should return all courses from the courses table.
+     * This implementation of the test will only actually test that *something* is returned,
+     * (i.e. that it returns something that is not empty), not that the correct data is returned.
+     *
+     */
     public function testGetAllCourses() {
         $courses = getAllCourses();
         $this->assertNotEmpty($courses);
     }
 
-    // create a new course called 'test_course', assert that the lastest entry 
-    // into the courses table has name 'test_course', then remove that entry.
+    /**
+     * Create a new course using the createCourse() function, then make sure that the name
+     * of the newly created course is the same as the latest course in the db.
+     */
     public function testCreateCourse() {
         createCourse("test course");
         $test_course = DB::run("SELECT * FROM courses ORDER BY course_id DESC LIMIT 1")->fetch(PDO::FETCH_ASSOC);
@@ -25,9 +38,10 @@ final class CoursesTest extends TestCase {
         $this->assertSame("test course", $test_course["name"]);
     }
 
-    // create a new course called 'test_course', assert that the lastest entry 
-    // into the courses table has the same course_id that getCourseById returns,
-    // then remove that entry
+    /**
+     * Get the test course from the db, then check if that course's id is the same as the
+     * course returned by the getCourseById() function.
+     */
     public function testGetCourseById() {
         $test_course = DB::run("SELECT * FROM courses WHERE name LIKE 'test course'")->fetch(PDO::FETCH_ASSOC);
         $test_course_id = $test_course['course_id'];
@@ -36,20 +50,26 @@ final class CoursesTest extends TestCase {
         $this->assertSame($test_course_id, $course['course_id']);
     }
 
+    /**
+     * Get the test course record from the db, ensure that the record exists,
+     * then run the deleteCourseById() method and assert that the record has been deleted
+     */
     public function testDeleteCourseById() {
         $test_course = DB::run("SELECT * FROM courses WHERE name LIKE 'test course'")->fetch(PDO::FETCH_ASSOC);
         $test_course_id = $test_course['course_id'];
-        // the record should exist, the number of columns in count should be 1
         $recordExists = DB::run("SELECT COUNT(1) FROM courses WHERE course_id = ?", [$test_course_id])->fetchColumn();
 
         $this->assertEquals(1, $recordExists);
         deleteCourseById($test_course_id);
 
-        // the record should be deleted, the number of columns in count should be 0
         $recordExists = DB::run("SELECT COUNT(1) FROM courses WHERE course_id = ?", [$test_course_id])->fetchColumn();
         $this->assertEquals(0, $recordExists);
     }
 
+    /**
+     * Remove the course on which the test methods are run.
+     * This is run after every test method.
+     */
     protected function tearDown(): void {
         DB::run("DELETE FROM courses WHERE name LIKE 'test course'");
     }
