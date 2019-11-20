6734f39d7ae4d02920a5dc07032cf1a9b5b61b54
Jack Bremner <j.z.bremner@dundee.ac.uk>
2019-11-20 10:22:55 +0000

    create Submissions test, fix bug in submission_functions found by test

--- submissions/submission_functions.php
+++ submissions/submission_functions.php
@@ -23,7 +23,7 @@ function createSubmission($coursework_id, $student_id, $mark = null, $hand_in_da
 
 function getSubmissionById($submission_id) {
 
-    $query = "SELECT mark, hand_in_date, grade, second_submission, name as student_name FROM submissions sub
+    $query = "SELECT submission_id, mark, hand_in_date, grade, second_submission, name as student_name FROM submissions sub
               JOIN students stu ON sub.student_id = stu.student_id
               WHERE submission_id = ?";
     $submission = DB::run($query, [$submission_id])->fetch(PDO::FETCH_ASSOC);
@@ -33,7 +33,7 @@ function getSubmissionById($submission_id) {
 function deleteSubmissionById($submission_id) {
     $query = "DELETE FROM submissions WHERE submission_id=?";
     $submission = DB::run($query, [$submission_id]);
-    return $course;
+    return $submission;
 }
 
 function getAlphanumericGrade($mark, $second_submission) {
